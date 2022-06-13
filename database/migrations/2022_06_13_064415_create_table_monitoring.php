6852f43b4cae4141ad25aa7ff2c36b714ccc64ba
adiapr <da801100@gmail.com>
2022-06-13 15:53:56 +0700

    Starter

---
+++ database/migrations/2022_06_13_064415_create_table_monitoring.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_monitoring', function (Blueprint $table) {
+            $table->id();
+            $table->string('project_name');
+            $table->string('client');
+            $table->string('leader_name');
+            $table->string('leader_email');
+            $table->string('leader_image');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('progress');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_monitoring');
+    }
+};
